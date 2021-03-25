98a363a335d7abcfa906859c16dcb503da1d95dc
seriouschess <rehayemb@gmail.com>
2021-03-25 15:25:16 -0500

    Removed template clutter

---
+++ Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jwtBearerTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        public MainController(){}
+
+        [HttpGet]
+        [Route("authenticate")]
+        public string Authenticate()
+        {
+            return "success";
+        }
+    }
+}
